8d026e1e50ce16aeb85d5a15228ba222f42afcc4
Nicholas Liu <liunicholas6@gmail.com>
2024-11-06 19:55:04 -0500

    Initial commit

---
+++ helper_cuda.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+
+#define checkCudaErrors(val) check((val), #val, __FILE__, __LINE__)
+
+template <typename T>
+void check(T result, char const *const func, const char *const file,
+           int const line) {
+  if (result) {
+    fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n", file, line,
+            static_cast<unsigned int>(result), cudaGetErrorName(result), func);
+    exit(EXIT_FAILURE);
+  }
+}
+
+#define getLastCudaError(msg) __getLastCudaError(msg, __FILE__, __LINE__)
+
+inline void __getLastCudaError(const char *errorMessage, const char *file,
+                               const int line) {
+  cudaError_t err = cudaGetLastError();
+
+  if (cudaSuccess != err) {
+    fprintf(stderr,
+            "%s(%i) : getLastCudaError() CUDA error :"
+            " %s : (%d) %s.\n",
+            file, line, errorMessage, static_cast<int>(err),
+            cudaGetErrorString(err));
+    exit(EXIT_FAILURE);
+  }
+}
